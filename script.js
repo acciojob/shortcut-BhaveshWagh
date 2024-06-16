77c24b819bbe9074b43309a511992f5c6e7cb63f
Bhavesh Wagh <bhaveshbm2711@gmail.com>
2024-06-16 13:54:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function shortcut(s1, s2) {
   // your code here
+	 if (str1 === '' || str2 === '') {
+        return '';
+    }
+
+    // Extract initial letters and convert to uppercase
+    let initial1 = str1[0].toUpperCase();
+    let initial2 = str2[0].toUpperCase();
+
+    // Return the concatenated initial letters
+    return initial1 + initial2;
 }
 
 // Do not change the code below.
